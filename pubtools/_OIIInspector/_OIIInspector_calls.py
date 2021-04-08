55de52d8910e09b83d518c7d287ce8b559026c08
xdaile <mychalze@gmail.com>
2021-04-08 14:50:27 +0200

    create call with arguments on method for getting list of index images
    Arguments created, method call also created, will need to update after method for working with arguments will be created

--- pubtools/_OIIInspector/_OIIInspector_calls.py
+++ pubtools/_OIIInspector/_OIIInspector_calls.py
@@ -8,7 +8,11 @@ GET_BUNDLE_IMAGE_OPERATORS_LIST_ARGS[("--name",)]={
     "type": str,
 }
 
-
+GET_INDEX_IMAGE_OPERATORS_LIST_ARGS[("--url",)]={
+    "help": "quay adrres of the index image",
+    "required":True,
+    "type": str,
+}
 
 def get_bundle_image_operators_list_main(sysargs=None):
     """
@@ -17,6 +21,22 @@ def get_bundle_image_operators_list_main(sysargs=None):
         dict: Metadata of the repository.
     """
     parser = setup_arg_parser(GET_BUNDLE_IMAGE_OPERATORS_LIST_ARGS)
+
+    if sysargs:
+        args = parser.parse_args(sysargs[1:])
+    else:
+        args = parser.parse_args()  # pragma: no cover"
+
+    return NotImplementedError    
+
+
+def get_index_image_operators_list_main(sysargs=None):
+    """
+    Entrypoint for getting repository metadata.
+    Returns:
+        dict: Metadata of the repository.
+    """
+    parser = setup_arg_parser(GET_INDEX_IMAGE_OPERATORS_LIST_ARGS)
 
     if sysargs:
         args = parser.parse_args(sysargs[1:])
