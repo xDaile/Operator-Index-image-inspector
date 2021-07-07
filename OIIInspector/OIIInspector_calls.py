134d73459ce2527d0b902d460d0ace91bfb9cb5d
xdaile <mychalze@gmail.com>
2021-07-07 22:33:42 +0200

    Implemented two new possible calls on OIIInspector
    Get bundle added - not tested yet
    List images function added

--- OIIInspector/OIIInspector_calls.py
+++ OIIInspector/OIIInspector_calls.py
@@ -11,6 +11,13 @@ ADDRESS_ARGS = {("--address",): {
 
 GET_INDEX_IMAGE_PACKAGES_LIST_ARGS = ADDRESS_ARGS.copy()
 
+GET_BUNDLE_ARGS = ADDRESS_ARGS.copy()
+GET_BUNDLE_FOR_CHANNEL_ARGS = ADDRESS_ARGS.copy()
+GET_BUNDLE_THAT_REPLACES_ARGS = ADDRESS_ARGS.copy()
+GET_DEFAULT_BUNDLE_THAT_PROVIDES_ARGS = ADDRESS_ARGS.copy()
+GET_PACKAGE_ARGS = ADDRESS_ARGS.copy()
+LIST_PACKAGES_ARGS = ADDRESS_ARGS.copy()
+
 
 def setup_oiii_client(args):
     return OIIIClient()
@@ -35,4 +42,39 @@ def get_index_image_packages_list_main(sysargs=None):
     return resp
 
 
-get_index_image_packages_list_main(sys.argv)
+def get_bundle_main(sysargs=None):
+    """
+    Entrypoint for getting bundle metadata.
+    Returns:
+        dict: Bundle image in the index image.
+    """
+    parser = setup_arg_parser(GET_INDEX_IMAGE_PACKAGES_LIST_ARGS)
+
+    if sysargs:
+        args = parser.parse_args(sysargs[1:])
+    else:
+        args = parser.parse_args()  # pragma: no cover"
+
+    oiii_client = setup_oiii_client(args)
+    resp = oiii_client.get_bundle(args.address)
+    json.dump(resp, sys.stdout, sort_keys=True, indent=4, separators=(",", ": "))
+    return resp
+
+
+def list_packages_main(sysargs=None):
+    """
+    Entrypoint for getting list of packages in index image.
+    Returns:
+        dict: List of packages in the index image.
+    """
+    parser = setup_arg_parser(GET_INDEX_IMAGE_PACKAGES_LIST_ARGS)
+
+    if sysargs:
+        args = parser.parse_args(sysargs[1:])
+    else:
+        args = parser.parse_args()  # pragma: no cover"
+
+    oiii_client = setup_oiii_client(args)
+    resp = oiii_client.list_packages(args.address)
+    json.dump(resp, sys.stdout, sort_keys=True, indent=4, separators=(",", ": "))
+    return resp
