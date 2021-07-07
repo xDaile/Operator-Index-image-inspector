072998f9fc34bfb03db94f16d741dedf334a79de
xdaile <mychalze@gmail.com>
2021-07-07 22:48:40 +0200

    Working with strings improved

--- OIIInspector/OIIIClient.py
+++ OIIInspector/OIIIClient.py
@@ -10,19 +10,29 @@ class OIIIClient:
         self.executor = LocalExecutor()
 
     def get_index_image_package_list(self, image_address):
-        command_to_call = "".join([self.grp_curl_position, " -plaintext ", image_address, " list"])
+        command_to_call = '{grp_curl_position} {call_type} {image_address} {call_argument}'.format(
+            grp_curl_position=self.grp_curl_position,
+            call_type="-plaintext",
+            image_address=image_address,
+            call_argument="list")
         out = self.executor.run_cmd(command_to_call, "Call failed to execute")
         return out.strip("\n").split("\n")
 
     def get_bundle(self, image_address):
-        command_to_call = "".join([self.grp_curl_position, " -plaintext ", image_address, " api.Registry/GetBundle"])
+        command_to_call = '{grp_curl_position} {call_type} {image_address} {call_argument}'.format(
+            grp_curl_position=self.grp_curl_position,
+            call_type="-plaintext",
+            image_address=image_address,
+            call_argument="api.Registry/GetBundle")
         out = self.executor.run_cmd(command_to_call, "Call failed to execute")
         return out.strip("\n").split("\n")
 
     def list_packages(self, image_address):
-        command_to_call = "".join([self.grp_curl_position, " -plaintext ", image_address, " api.Registry/ListPackages"])
+        command_to_call = '{grp_curl_position} {call_type} {image_address} {call_argument}'.format(
+            grp_curl_position=self.grp_curl_position,
+            call_type="-plaintext",
+            image_address=image_address,
+            call_argument="api.Registry/ListPackages")
         out = self.executor.run_cmd(command_to_call, "Call failed to execute")
-        out = out.replace("\n", "")
-        out = out.replace("}", "},")
-        out = ast.literal_eval(out)
-        return out
+        out = out.replace("\n", "").replace("}", "},")
+        return ast.literal_eval(out)
