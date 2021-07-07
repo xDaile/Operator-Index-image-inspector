e638d4a8215287ea8c9947e72933abbb875d052d
xdaile <mychalze@gmail.com>
2021-07-07 22:29:52 +0200

    Module for importing grpCurl address

--- OIIInspector/GrpCurlAddressImporter.py
+++ OIIInspector/GrpCurlAddressImporter.py
@@ -1,9 +1,11 @@
 import os
+import logging
 
+LOG = logging.getLogger("OIIInspector")
 
 class GrpCurlAddressImporter:
     grp_curl_address = "./grpCurl/grpcurl"
 
     def __init__(self):
         if not(os.path.exists(self.grp_curl_address)):
-            print("grpCurl does not exists")
+            LOG.error("directory ./OIIInspector/grpCurl does not containt binary called grpcurl".format(cmd, err))
