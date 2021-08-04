c5a7ceddda07cb0199fdc1b898afac40a7eb9aa2
xdaile <mychalze@gmail.com>
2021-08-04 11:17:48 +0200

    Rename variable api_answer to api_response

--- OIIInspector/OutputAPIConvertor.py
+++ OIIInspector/OutputAPIConvertor.py
@@ -5,23 +5,23 @@ import re
 LOG = logging.getLogger("OIIInspector")
 
 
-def convert_output(api_answer):
+def convert_output(api_response):
     """
     Converts answer from API into JSON
     Args:
-        api_answer (str):
+        api_response (str):
             String received from grpcurl call on api.Registry
 
     Returns (str)
         String already convertible into JSON format.
     """
-    if api_answer == "":
+    if api_response == "":
         LOG.error("Answer from API is empty")
         raise RuntimeError("Answer from API is corrupted")
 
-    api_answer = re.sub(r'}\s*{', "} ,{", api_answer)
+    api_response = re.sub(r'}\s*{', "} ,{", api_response)
 
-    json_data = json.loads(api_answer)
+    json_data = json.loads(api_response)
     if 'csvJson' in json_data:
         json_data['csvJson'] = json.loads(json_data['csvJson'])
 
