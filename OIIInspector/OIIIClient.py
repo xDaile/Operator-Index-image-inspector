20a2a5d768b870e975a4b32b95a3d0cdc0708a81
xdaile <mychalze@gmail.com>
2021-09-20 19:36:25 +0200

    Removed excess function
    Function get_index_image_api_endpoints_list was redundant in this project. Trimmed whitespaces, improved imports.

--- OIIInspector/OIIIClient.py
+++ OIIInspector/OIIIClient.py
@@ -1,5 +1,5 @@
-from LocalExecutor import run_cmd
-from OutputAPIConvertor import convert_output
+import OIIInspector.utils as utils
+
 import json
 
 
@@ -7,14 +7,14 @@ class OIIIClient:
 
     terminal_command = 'grpcurl -plaintext {call_argument} {image_address} {api_address}'
 
-    def get_index_image_api_endpoints_list(self, image_address):
-        command_to_call = self.terminal_command.format(
-                            call_argument="",
-                            image_address=image_address,
-                            api_address="list")
-        out = run_cmd(command_to_call)
-        list_of_index_images = out.split("\n")[0:-1]
-        return convert_output(json.dumps(list_of_index_images))
+    # def get_index_image_api_endpoints_list(self, image_address):
+    #     command_to_call = self.terminal_command.format(
+    #                         call_argument="",
+    #                         image_address=image_address,
+    #                         api_address="list")
+    #     out = utils.run_cmd(command_to_call)
+    #     list_of_index_images = out.split("\n")[0:-1]
+    #     return utils.convert_output(json.dumps(list_of_index_images))
 
     def get_bundle(self, image_address, pkg_name, channel_name, csv_name):
         call_argument = '-d \'{{\"pkgName\":\"{package_name}\",' \
@@ -23,23 +23,25 @@ class OIIIClient:
                             package_name=pkg_name,
                             channel_name=channel_name,
                             csv_name=csv_name)
+
         command_to_call = self.terminal_command.format(
                             call_argument=call_argument,
                             image_address=image_address,
                             api_address="api.Registry/GetBundle")
-        out = run_cmd(command_to_call)
-        return convert_output(out)
+
+        out = utils.run_cmd(command_to_call)
+        return utils.convert_output(out)
 
     def list_packages(self, image_address):
         command_to_call = self.terminal_command.format(
                             call_argument="",
                             image_address=image_address,
                             api_address="api.Registry/ListPackages")
-        out = run_cmd(command_to_call)
+        out = utils.run_cmd(command_to_call)
         out = "{{\"data\":[ {result_api} ]}}".format(result_api=out)
         json_data = []
-        for part in convert_output(out)["data"]:
-            json_data.append(convert_output(json.dumps(part)))
+        for part in utils.convert_output(out)["data"]:
+            json_data.append(utils.convert_output(json.dumps(part)))
         return json_data
 
     def list_bundles(self, image_address):
@@ -47,11 +49,11 @@ class OIIIClient:
                             call_argument="",
                             image_address=image_address,
                             api_address="api.Registry/ListBundles")
-        out = run_cmd(command_to_call)
+        out = utils.run_cmd(command_to_call)
         out = "{{\"data\":[ {result_api} ]}}".format(result_api=out)
         json_data = []
-        for part in convert_output(out)["data"]:
-            json_data.append(convert_output(json.dumps(part)))
+        for part in utils.convert_output(out)["data"]:
+            json_data.append(utils.convert_output(json.dumps(part)))
         return json_data
 
     def get_package(self, image_address, package_name):
@@ -61,8 +63,8 @@ class OIIIClient:
                             call_argument=call_argument,
                             image_address=image_address,
                             api_address="api.Registry/GetPackage")
-        out = run_cmd(command_to_call)
-        return convert_output(out)
+        out = utils.run_cmd(command_to_call)
+        return utils.convert_output(out)
 
     def get_bundle_for_channel(self, image_address, package_name, channel_name):
         call_argument = '-d \'{{\"pkgName\":\"{package_name}\", ' \
@@ -73,8 +75,8 @@ class OIIIClient:
                             call_argument=call_argument,
                             image_address=image_address,
                             api_address="api.Registry/GetBundleForChannel")
-        out = run_cmd(command_to_call)
-        return convert_output(out)
+        out = utils.run_cmd(command_to_call)
+        return utils.convert_output(out)
 
     def get_bundle_that_replaces(self, image_address, package_name, channel_name, csv_name):
         call_argument = '-d \'{{\"pkgName\":\"{package_name}\",' \
@@ -87,8 +89,8 @@ class OIIIClient:
                             call_argument=call_argument,
                             image_address=image_address,
                             api_address="api.Registry/GetBundleThatReplaces")
-        out = run_cmd(command_to_call)
-        return convert_output(out)
+        out = utils.run_cmd(command_to_call)
+        return utils.convert_output(out)
 
     def get_default_bundle_that_provides(self, image_address, group, version, kind, plural):
         call_argument = '-d \'{{\"group\":\"{group}\",' \
@@ -103,5 +105,5 @@ class OIIIClient:
                             call_argument=call_argument,
                             image_address=image_address,
                             api_address="api.Registry/GetDefaultBundleThatProvides")
-        out = run_cmd(command_to_call)
-        return convert_output(out)
+        out = utils.run_cmd(command_to_call)
+        return utils.convert_output(out)
