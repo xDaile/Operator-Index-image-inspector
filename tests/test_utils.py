e485206f43bef46a7456408b0b396f8cccb6963a
xdaile <mychalze@gmail.com>
2021-09-28 22:46:13 +0200

    Asserts improved, added with statement to file handling

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,16 +12,22 @@ mock_process = MagicMock()
 mock_process.communicate.return_value = ["success".encode('utf-8'), 0]
 
 
+def load_and_convert_file(file_name):
+    test_file_name = input_file_name.format(test_name=file_name)
+    with open(test_file_name, "r") as file:
+        input_data = file.read()
+    return convert_output(input_data)
+
+
 @patch("subprocess.Popen")
 def test_run_cmd_pass(mocked_popen):
     mock_process.returncode = 0
     mocked_popen.return_value = mock_process
     output = run_cmd("ls")
     assert output == "success"
-    mocked_popen.assert_called_with(["ls"], stdout=-1)
+    mocked_popen.assert_called_once_with(["ls"], stdout=-1)
 
 
-# during run, error occurs and returned value is 1
 @patch("subprocess.Popen")
 def test_run_cmd_fail(mocked_popen):
     mock_process.returncode = 1
@@ -29,35 +35,36 @@ def test_run_cmd_fail(mocked_popen):
     with pytest.raises(RuntimeError):
         output = run_cmd("ls")
         assert output == "success"
-    mocked_popen.assert_called_with(["ls"], stdout=-1)
+    mocked_popen.assert_called_once_with(["ls"], stdout=-1)
 
 
 def test_convert_output_combined():
+
     converted_input = load_and_convert_file("convert_output_combined.json")
     assert 'csvJson' in converted_input
     assert 'spec' in converted_input
     assert 'object' in converted_input
-    assert len(converted_input['csvJson']) is CONVERT_OUTPUT_CSVJSON_OBJECTS_COUNT
-    assert len(converted_input['object']) is CONVERT_OUTPUT_OBJECT_LIST_OBJECTS_COUNT
-    assert len(converted_input['spec']) is CONVERT_OUTPUT_SPEC_JSON_OBJECTS_COUNT
+    assert len(converted_input['csvJson']) == CONVERT_OUTPUT_CSVJSON_OBJECTS_COUNT
+    assert len(converted_input['object']) == CONVERT_OUTPUT_OBJECT_LIST_OBJECTS_COUNT
+    assert len(converted_input['spec']) == CONVERT_OUTPUT_SPEC_JSON_OBJECTS_COUNT
 
 
 def test_convert_output_csvjson():
     converted_input = load_and_convert_file("convert_output_csvjson.json")
     assert 'csvJson' in converted_input
-    assert len(converted_input['csvJson']) is CONVERT_OUTPUT_CSVJSON_OBJECTS_COUNT
+    assert len(converted_input['csvJson']) == CONVERT_OUTPUT_CSVJSON_OBJECTS_COUNT
 
 
 def test_convert_output_object_list():
     converted_input = load_and_convert_file("convert_output_object_list.json")
     assert 'object' in converted_input
-    assert len(converted_input['object']) is CONVERT_OUTPUT_OBJECT_LIST_OBJECTS_COUNT
+    assert len(converted_input['object']) == CONVERT_OUTPUT_OBJECT_LIST_OBJECTS_COUNT
 
 
 def test_convert_output_spec():
     converted_input = load_and_convert_file("convert_output_spec.json")
     assert 'spec' in converted_input
-    assert len(converted_input['spec']) is CONVERT_OUTPUT_SPEC_JSON_OBJECTS_COUNT
+    assert len(converted_input['spec']) == CONVERT_OUTPUT_SPEC_JSON_OBJECTS_COUNT
 
 
 def test_convert_output_empty_input():
@@ -72,12 +79,6 @@ def test_bad_input():
         load_and_convert_file("convert_output_corrupted_format_2.json")
     with pytest.raises(json.decoder.JSONDecodeError):
         load_and_convert_file("convert_output_corrupted_format_3.json")
-
-
-def load_and_convert_file(file_name):
-    test_file_name = input_file_name.format(test_name=file_name)
-    input_data = open(test_file_name, "r").read()
-    return convert_output(input_data)
 
 
 def test_parser_arg_groups(capsys):
