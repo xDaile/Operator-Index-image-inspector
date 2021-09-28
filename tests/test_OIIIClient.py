e76de4fb3f30d7b25efbb76e5e5fb67d4eba7b2c
xdaile <mychalze@gmail.com>
2021-09-28 22:47:00 +0200

    Asserts improved
    Called_once_with instead of called_once and called_with

--- tests/test_OIIIClient.py
+++ tests/test_OIIIClient.py
@@ -22,7 +22,7 @@ def test_command_building():
 def test_get_bundle(mock_run_cmd):
     output = OIIIClient_object.get_bundle("localhost:50051", "serverless-operator", "4.3",
                                           "serverless-operator.v1.2.0")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext -d "
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext -d "
                                     "\'{\"pkgName\":\"serverless-operator\","
                                     "\"channelName\":\"4.3\","
                                     "\"csvName\":\"serverless-operator.v1.2.0\"}\' "
@@ -30,26 +30,21 @@ def test_get_bundle(mock_run_cmd):
                                     "api.Registry/GetBundle")
     assert output["version"] == "1.2.0"
     assert output["providedApis"][0]["plural"] == "test_plural"
-    mock_run_cmd.assert_called_once()
-    # feel like the outputs from OIIClient should be tested if it is in valid form,
-    # but i am not sure if it is possible to break it in OIIIClient in any way, I tried, did not succeeded
-    json.loads(json.dumps(output))
 
 
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("list_packages.json"))
 def test_list_packages(mock_run_cmd):
     output = OIIIClient_object.list_packages("localhost:50051")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext  "
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext  "
                                     "localhost:50051 "
                                     "api.Registry/ListPackages")
     assert output[0]["name"] == "test-operator"
-    mock_run_cmd.assert_called_once()
 
 
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("list_bundles.json"))
 def test_list_bundles(mock_run_cmd):
     output = OIIIClient_object.list_bundles("localhost:50051")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext  localhost:50051 api.Registry/ListBundles")
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext  localhost:50051 api.Registry/ListBundles")
     assert output[0]["csvName"] == "test_csv_name 1"
     assert output[0]["object"][2]["apiVersion"] == "test_api_version_0"
     assert output[1]["csvName"] == "test_csv_name 2"
@@ -59,33 +54,31 @@ def test_list_bundles(mock_run_cmd):
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("get_package.json"))
 def test_get_package(mock_run_cmd):
     output = OIIIClient_object.get_package("localhost:50051", "serverless-operator")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext -d "
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext -d "
                                     "\'{\"name\":\"serverless-operator\"}\' "
                                     "localhost:50051 "
                                     "api.Registry/GetPackage")
     assert output["name"] == "test-operator"
     assert output["channels"][3]["csvName"] == "test-operator.v1.10.0"
-    mock_run_cmd.assert_called_once()
 
 
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("get_bundle_for_channel.json"))
 def test_get_bundle_for_channel(mock_run_cmd):
     output = OIIIClient_object.get_bundle_for_channel("localhost:50051", "submariner", "alpha")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext -d "
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext -d "
                                     "\'{\"pkgName\":\"submariner\", "
                                     "\"channelName\":\"alpha\"}\' "
                                     "localhost:50051 "
                                     "api.Registry/GetBundleForChannel")
     assert output["csvName"] == "test-name.v0.8.1"
     assert output["object"][8]["kind"] == "ClusterServiceVersion"
-    mock_run_cmd.assert_called_once()
 
 
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("get_bundle_that_replaces.json"))
 def test_get_bundle_that_replaces(mock_run_cmd):
     output = OIIIClient_object.get_bundle_that_replaces("localhost:50051", "submariner", "alpha",
                                                         "beta")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext -d "
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext -d "
                                     "\'{\"pkgName\":\"submariner\", "
                                     "\"channelName\":\"alpha\","
                                     "\"csvName\":\"beta\"}\' "
@@ -93,14 +86,13 @@ def test_get_bundle_that_replaces(mock_run_cmd):
                                     "api.Registry/GetBundleThatReplaces")
     assert output["csvName"] == "test-operator.v1.3.0"
     assert output["providedApis"][0]["plural"] == "test_plural"
-    mock_run_cmd.assert_called_once()
 
 
 @patch("OIIInspector.utils.run_cmd", return_value=load_file("get_default_bundle_that_provides.json"))
 def test_get_default_bundle_that_provides(mock_run_cmd):
     output = OIIIClient_object.get_default_bundle_that_provides("localhost:50051", "testGroup",
                                                                 "testVersion", "testKind", "testPlural")
-    mock_run_cmd.assert_called_with("grpcurl -plaintext -d \'{"
+    mock_run_cmd.assert_called_once_with("grpcurl -plaintext -d \'{"
                                     "\"group\":\"testGroup\", "
                                     "\"version\":\"testVersion\","
                                     "\"kind\":\"testKind\","
@@ -110,4 +102,3 @@ def test_get_default_bundle_that_provides(mock_run_cmd):
     assert output["csvName"] == "test-operator.v8.2.0"
     assert output["providedApis"][4]["plural"] == "caches"
     assert output["object"][5]["kind"] == "CustomResourceDefinition"
-    mock_run_cmd.assert_called_once()
